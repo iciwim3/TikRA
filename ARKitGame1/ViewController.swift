c2fedc70f817ec4a0c0d1425fe2612f25c4e30c9
SKE <appybuildmore@gmail.com>
2017-07-12 12:25:09 -0500

    Initial Commit

---
+++ ARKitGame1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitGame1
+//
+//  Created by Sain-R Edwards Jr. on 7/12/17.
+//  Copyright © 2017 Sain-R Edwards Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
